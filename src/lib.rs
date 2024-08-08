f7172f73262a0e7a8688022869c1305692b9973d
Devin Jean <emailcruzjean@yahoo.com>
2024-08-08 05:33:00 -0500

    new cvt

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![forbid(unsafe_code)]
+#![no_implicit_prelude]
+#![no_std]
+
 /// This type is only needed to bypass a limitation of the current (stable) trait bound system.
 /// See: https://github.com/taiki-e/pin-project/issues/102#issuecomment-540472282
 pub struct Wrapper<'a, T>(T, ::core::marker::PhantomData<&'a ()>);
@@ -9,12 +13,12 @@ impl<'a, T> Wrapper<'a, T> {
         &self.0
     }
 }
-impl<T: Clone> ::core::clone::Clone for Wrapper<'_, T> {
+impl<T: ::core::clone::Clone> ::core::clone::Clone for Wrapper<'_, T> {
     fn clone(&self) -> Self {
         Self(::core::clone::Clone::clone(&self.0), ::core::marker::PhantomData)
     }
 }
-impl<T: Copy> ::core::marker::Copy for Wrapper<'_, T> {}
+impl<T: ::core::marker::Copy> ::core::marker::Copy for Wrapper<'_, T> {}
 impl<T: ::core::fmt::Debug> ::core::fmt::Debug for Wrapper<'_, T> {
     fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
         ::core::fmt::Debug::fmt(&self.0, f)
@@ -32,7 +36,7 @@ impl<U, T: ::core::cmp::PartialEq<U>> ::core::cmp::PartialEq<Wrapper<'_, U>> for
 }
 impl<T: ::core::cmp::Eq> ::core::cmp::Eq for Wrapper<'_, T> {}
 impl<U, T: ::core::cmp::PartialOrd<U>> ::core::cmp::PartialOrd<Wrapper<'_, U>> for Wrapper<'_, T> {
-    fn partial_cmp(&self, other: &Wrapper<'_, U>) -> Option<::core::cmp::Ordering> {
+    fn partial_cmp(&self, other: &Wrapper<'_, U>) -> ::core::option::Option<::core::cmp::Ordering> {
         ::core::cmp::PartialOrd::partial_cmp(&self.0, &other.0)
     }
 }
@@ -43,24 +47,34 @@ impl<T: ::core::cmp::Ord> ::core::cmp::Ord for Wrapper<'_, T> {
 }
 
 /// An abstract unit of measure.
-pub trait Unit: ::core::clone::Clone + ::core::convert::From<Self::Base> {
+pub trait Unit: ::core::clone::Clone {
     /// The raw type used to represent the numeric value of this unit.
     type Raw;
     /// The type this unit is based on (possibly itself).
-    type Base: Unit + ::core::convert::From<Self>;
+    type Base: Unit<Raw = Self::Raw, Base = Self::Base>;
 
     /// Constructs a new instance of this unit from a raw numeric value.
     fn new(v: Self::Raw) -> Self;
     /// Gets the raw numeric value of this unit.
     fn raw(&self) -> &Self::Raw;
+
+    /// Constructs a derived unit from its base unit.
+    fn from_base(v: Self::Base) -> Self;
+    /// Converts a derived unit into its base unit.
+    fn into_base(self) -> Self::Base;
+
+    /// Converts this unit into a compatible unit.
+    fn convert<U: Unit<Raw = Self::Raw, Base = Self::Base>>(self) -> U {
+        U::from_base(self.into_base())
+    }
 }
 
 #[macro_export]
 macro_rules! make_unit {
     ($vis:vis $name:ident : $t:ty) => {
-        $crate::make_unit! { $vis $name : $t, base = $name }
+        $crate::make_unit! { $vis $name : $t, base = $name, from_base = |x| x, into_base = |x| x }
     };
-    ($vis:vis $name:ident : $t:ty, base = $base:ty) => {
+    ($vis:vis $name:ident : $t:ty, base = $base:ty, from_base = $from_base:expr, into_base = $into_base:expr $(,)?) => {
         $vis struct $name($crate::Wrapper<'static, $t>);
         impl $crate::Unit for $name {
             type Raw = $t;
@@ -72,39 +86,33 @@ macro_rules! make_unit {
             fn raw(&self) -> &$t {
                 &self.0.raw()
             }
+
+            fn from_base(v: $base) -> Self {
+                $from_base(v)
+            }
+            fn into_base(self) -> $base {
+                $into_base(self)
+            }
         }
         impl ::core::clone::Clone for $name where for<'a> $crate::Wrapper<'a, $t>: ::core::clone::Clone {
             fn clone(&self) -> Self {
-                Self(self.0.clone())
+                Self(::core::clone::Clone::clone(&self.0))
             }
         }
         impl ::core::marker::Copy for $name where for<'a> $crate::Wrapper<'a, $t>: ::core::marker::Copy { }
         impl ::core::fmt::Debug for $name where for<'a> $crate::Wrapper<'a, $t>: ::core::fmt::Debug {
             fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
-                self.0.fmt(f)
+                ::core::fmt::Debug::fmt(&self.0, f)
             }
         }
         impl ::core::fmt::Display for $name where for<'a> $crate::Wrapper<'a, $t>: ::core::fmt::Display {
             fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
-                self.0.fmt(f)
+                ::core::fmt::Display::fmt(&self.0, f)
             }
         }
-        // impl<U: $crate::Unit<Raw = $t, Base = $base>> ::core::cmp::PartialEq<U> for $name where for<'a> $crate::Wrapper<'a, $t>: ::core::cmp::PartialEq {
-        //     fn eq(&self, other: &U) -> bool {
-        //         $crate::Wrapper::new(<$base>::from(self.clone())).eq(&$crate::Wrapper::new(<<U as $crate::Unit>::Base as From<U>>::from(other.clone())))
-        //     }
-        // }
-    };
-    ($vis:vis $name:ident : $base:ident, from_base = $from_base:expr, to_base = $to_base:expr $(,)?) => {
-        $crate::make_unit! { $vis $name : <$base as $crate::Unit>::Raw, base = <$base as $crate::Unit>::Base }
-        impl ::core::convert::From<$base> for $name {
-            fn from(v: $base) -> $name {
-                $from_base(v)
-            }
-        }
-        impl ::core::convert::From<$name> for $base {
-            fn from(v: $name) -> $base {
-                $to_base(v)
+        impl<U: $crate::Unit<Raw = $t, Base = $base>> ::core::cmp::PartialEq<U> for $name where for<'a> $crate::Wrapper<'a, $t>: ::core::cmp::PartialEq {
+            fn eq(&self, other: &U) -> bool {
+                <Self as $crate::Unit>::into_base(::core::clone::Clone::clone(self)).0.eq(&<U as $crate::Unit>::into_base(::core::clone::Clone::clone(other)).0)
             }
         }
     };
--- tests/test.rs
+++ tests/test.rs
@@ -1,61 +1,81 @@
-meta_units::make_unit! { Meters : f64 }
-meta_units::make_unit! {
-    Kilometers : Meters,
-    from_base = |x: Meters| <Kilometers as meta_units::Unit>::new(meta_units::Unit::raw(&x) / 1000.0),
-    to_base = |x: Kilometers| <Meters as meta_units::Unit>::new(meta_units::Unit::raw(&x) * 1000.0),
+#![no_implicit_prelude]
+
+::meta_units::make_unit! { Meters : f64 }
+::meta_units::make_unit! {
+    Kilometers : f64,
+    base = Meters,
+    from_base = |x: Meters| <Kilometers as ::meta_units::Unit>::new(::meta_units::Unit::raw(&x) / 1e3),
+    into_base = |x: Kilometers| <Meters as ::meta_units::Unit>::new(::meta_units::Unit::raw(&x) * 1e3),
+}
+::meta_units::make_unit! {
+    Megameters : f64,
+    base = Meters,
+    from_base = |x: Meters| <Megameters as ::meta_units::Unit>::new(::meta_units::Unit::raw(&x) / 1e6),
+    into_base = |x: Megameters| <Meters as ::meta_units::Unit>::new(::meta_units::Unit::raw(&x) * 1e6),
 }
 
 #[test]
 fn test_no_traits() {
     #[derive(Clone)]
     struct Thing;
-    meta_units::make_unit! { Meters : Thing }
+    ::meta_units::make_unit! { Meters : Thing }
 }
 
 #[test]
 fn test_basic_1() {
-    let x = <Meters as meta_units::Unit>::new(10.0);
-    let y = x.clone();
-    println!("{} {}", meta_units::Unit::raw(&x), meta_units::Unit::raw(&y));
+    let x = <Meters as ::meta_units::Unit>::new(10.0);
+    let y = ::core::clone::Clone::clone(&x);
+    ::std::println!("{} {}", ::meta_units::Unit::raw(&x), ::meta_units::Unit::raw(&y));
 
-    let x = <Meters as meta_units::Unit>::new(10.0);
+    let x = <Meters as ::meta_units::Unit>::new(10.0);
     let y = x;
-    println!("{} {}", meta_units::Unit::raw(&x), meta_units::Unit::raw(&y));
+    ::std::println!("{} {}", ::meta_units::Unit::raw(&x), ::meta_units::Unit::raw(&y));
 
-    assert_eq!(format!("{x}"), format!("{}", meta_units::Unit::raw(&x)));
-    assert_eq!(format!("{x:?}"), format!("{:?}", meta_units::Unit::raw(&x)));
+    ::core::assert_eq!(::std::format!("{x}"), ::std::format!("{}", ::meta_units::Unit::raw(&x)));
+    ::core::assert_eq!(::std::format!("{x:?}"), ::std::format!("{:?}", ::meta_units::Unit::raw(&x)));
 
-    // assert_eq!(x, y);
-    // assert_eq!(x, <Meters as meta_units::Unit>::new(10.0));
-    // assert_ne!(x, <Meters as meta_units::Unit>::new(10.1));
+    ::core::assert_eq!(x, y);
+    ::core::assert_eq!(x, <Meters as ::meta_units::Unit>::new(10.0));
+    ::core::assert_ne!(x, <Meters as ::meta_units::Unit>::new(10.1));
 }
 
 #[test]
 fn test_basic_2() {
-    let x = <Kilometers as meta_units::Unit>::new(10.0);
-    let y = x.clone();
-    println!("{} {}", meta_units::Unit::raw(&x), meta_units::Unit::raw(&y));
+    let x = <Kilometers as ::meta_units::Unit>::new(10.0);
+    let y = ::core::clone::Clone::clone(&x);
+    ::std::println!("{} {}", ::meta_units::Unit::raw(&x), ::meta_units::Unit::raw(&y));
 
-    let x = <Kilometers as meta_units::Unit>::new(10.0);
+    let x = <Kilometers as ::meta_units::Unit>::new(10.0);
     let y = x;
-    println!("{} {}", meta_units::Unit::raw(&x), meta_units::Unit::raw(&y));
+    ::std::println!("{} {}", ::meta_units::Unit::raw(&x), ::meta_units::Unit::raw(&y));
 
-    assert_eq!(format!("{x}"), format!("{}", meta_units::Unit::raw(&x)));
-    assert_eq!(format!("{x:?}"), format!("{:?}", meta_units::Unit::raw(&x)));
+    ::core::assert_eq!(::std::format!("{x}"), ::std::format!("{}", ::meta_units::Unit::raw(&x)));
+    ::core::assert_eq!(::std::format!("{x:?}"), ::std::format!("{:?}", ::meta_units::Unit::raw(&x)));
 
-    // assert_eq!(x, y);
-    // assert_eq!(x, <Kilometers as meta_units::Unit>::new(10.0));
-    // assert_ne!(x, <Kilometers as meta_units::Unit>::new(10.1));
+    ::core::assert_eq!(x, y);
+    ::core::assert_eq!(x, <Kilometers as ::meta_units::Unit>::new(10.0));
+    ::core::assert_ne!(x, <Kilometers as ::meta_units::Unit>::new(10.1));
 }
 
 #[test]
 fn test_conversion_1() {
-    let x = <Meters as meta_units::Unit>::new(2000.0);
-    let y: Kilometers = x.into();
-    let z: Meters = y.into();
-    assert_eq!(*meta_units::Unit::raw(&x), 2000.0);
-    assert_eq!(*meta_units::Unit::raw(&y), 2.0);
-    assert_eq!(*meta_units::Unit::raw(&z), 2000.0);
+    let x = <Meters as ::meta_units::Unit>::new(2000.0);
+    let y: Kilometers = <Meters as ::meta_units::Unit>::convert(x);
+    let z: Meters = <Kilometers as ::meta_units::Unit>::convert(y);
+    ::core::assert_eq!(*::meta_units::Unit::raw(&x), 2000.0);
+    ::core::assert_eq!(*::meta_units::Unit::raw(&y), 2.0);
+    ::core::assert_eq!(*::meta_units::Unit::raw(&z), 2000.0);
 
-    // assert_eq!(<Meters as meta_units::Unit>::new(2500.0), <Kilometers as meta_units::Unit>::new(2.5));
+    let x = <Kilometers as ::meta_units::Unit>::new(2000.0);
+    let y: Megameters = <Kilometers as ::meta_units::Unit>::convert(x);
+    let z: Kilometers = <Megameters as ::meta_units::Unit>::convert(y);
+    ::core::assert_eq!(*::meta_units::Unit::raw(&x), 2000.0);
+    ::core::assert_eq!(*::meta_units::Unit::raw(&y), 2.0);
+    ::core::assert_eq!(*::meta_units::Unit::raw(&z), 2000.0);
+
+    ::core::assert_eq!(<Meters as ::meta_units::Unit>::new(2500.0), <Kilometers as ::meta_units::Unit>::new(2.5));
+    ::core::assert_eq!(<Kilometers as ::meta_units::Unit>::new(2500.0), <Megameters as ::meta_units::Unit>::new(2.5));
+
+    ::core::assert_ne!(<Meters as ::meta_units::Unit>::new(2500.0), <Kilometers as ::meta_units::Unit>::new(2.51));
+    ::core::assert_ne!(<Kilometers as ::meta_units::Unit>::new(2500.0), <Megameters as ::meta_units::Unit>::new(2.51));
 }
